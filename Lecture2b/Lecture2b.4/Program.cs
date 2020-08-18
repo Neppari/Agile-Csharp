0735c5c424a4b99da9b6fc805aead72704a24079
Meri Mattila <meri-mattila@outlook.com>
2020-08-18 11:53:09 +0300

    Lecture 2

---
+++ Lecture2b/Lecture2b.4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TestNamespace;
+
+namespace Lecture2b._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass();
+        }
+    }
+}
